a4c1f96f6ca831b98f6da4afce0df387f631986b
yossularko <yossularko@gmail.com>
2025-05-01 10:47:34 +0700

    add: query get pokemon details

--- app/(app)/details/[id].tsx
+++ app/(app)/details/[id].tsx
@@ -1,16 +1,59 @@
 import { useLocalSearchParams } from "expo-router";
 import { useSetAtom } from "jotai";
-import { useEffect } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
 import { detailIdAtom } from "../_layout";
+import { useQuery } from "@apollo/client";
+import {
+  GET_POKEMON_DETAIL,
+  GET_POKEMON_SPECIES_DETAIL,
+} from "@/utils/queries/getPokemon";
+import client from "@/lib/apolloClient";
 
 export default function DetailsScreen() {
   const { id } = useLocalSearchParams();
   const setId = useSetAtom(detailIdAtom);
+  const [speciesId, setSpeciesId] = useState(0);
+
+  const { loading, error, data } = useQuery(GET_POKEMON_DETAIL, {
+    variables: { id: 1 },
+    client: client,
+  });
+
+  const {
+    loading: loadingSpecies,
+    error: errorSpecies,
+    data: dataSpecies,
+  } = useQuery(GET_POKEMON_SPECIES_DETAIL, {
+    variables: { speciesId: speciesId },
+    client: client,
+    skip: speciesId ? false : true,
+  });
+
+  useEffect(() => {
+    if (data) {
+      if (data.pokemon_v2_pokemon !== 0) {
+        const newData = data.pokemon_v2_pokemon[0];
+        setSpeciesId(newData?.pokemon_v2_pokemonspecy?.id || 0);
+      }
+
+      console.log("data details: ", data);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (dataSpecies) {
+      console.log("data species: ", dataSpecies);
+    }
+  }, [dataSpecies]);
 
   useEffect(() => {
     setId(id as string);
   }, [id]);
+
+  if (loading || loadingSpecies) return <ActivityIndicator />;
+  if (error || errorSpecies)
+    return <Text>Error! {error?.message || errorSpecies?.message}</Text>;
 
   return (
     <View style={styles.container}>
--- utils/queries/getPokemon.ts
+++ utils/queries/getPokemon.ts
@@ -22,3 +22,47 @@ export const GET_POKEMON_LIST = gql`
     }
   }
 `
+export const GET_POKEMON_DETAIL = gql`
+  query GetPokemonDetail($id: Int!) {
+    pokemon_v2_pokemon(where: {id: {_eq: $id}}) {
+      id
+      name
+      height
+      weight
+      pokemon_v2_pokemontypes {
+        pokemon_v2_type {
+          name
+        }
+      }
+      pokemon_v2_pokemonstats {
+        base_stat
+        pokemon_v2_stat {
+          name
+        }
+      }
+      pokemon_v2_pokemonabilities {
+        pokemon_v2_ability {
+          name
+        }
+      }
+      pokemon_v2_pokemonspecy {
+        id
+      }
+    }
+  }
+`
+
+export const GET_POKEMON_SPECIES_DETAIL = gql`
+  query GetPokemonSpeciesDetail($speciesId: Int!) {
+    pokemon_v2_pokemonspecies(where: {id: {_eq: $speciesId}}) {
+      id
+      capture_rate
+      gender_rate
+      growth_rate_id
+      hatch_counter
+      pokemon_v2_pokemonspeciesflavortexts(where: {language_id: {_eq: 9}}, limit: 1) {
+        flavor_text
+      }
+    }
+  }
+`
